8c037e1d46a32b2be4a35e1b2168d67f76aae666
mohammad-zeynali <mhmdzynaly977@gmail.com>
2024-04-08 13:01:08 +0330

    domain product has bin devloped

---
+++ src/domain/Product.ts
@@ -0,0 +1,25 @@
+export type Product = {
+  id: UniqId;
+  title: string;
+  price: number;
+  image: string;
+  area: string;
+  description: string;
+};
+export type AllProducts = Product[];
+
+export const addProductAds = (products: AllProducts, product: Product) => {
+  return { ...products, products: [...products, product] };
+};
+
+export const searchProduct = (title: string, products: AllProducts) => {
+  return products.filter((product) => product.title === title);
+};
+
+export const sortingProductByPrice = (price: number, products: AllProducts) => {
+  return products.filter((product) => product.price < price);
+};
+
+export const sortingProductByArea = (area: string, products: AllProducts) => {
+  return products.filter((product) => product.area === area);
+};
